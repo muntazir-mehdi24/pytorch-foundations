5494fce5be750bdb22343e512e3abd33d2631fce
Muntazir <muntazirjafri464@gmail.com>
2026-09-12 17:03:55 +0530

    Complete Torch.nn MLP

--- xor_module_mlp.py
+++ xor_module_mlp.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.optim as optim
 
 class XOR_MLP(nn.Module):
 
@@ -10,9 +11,59 @@ class XOR_MLP(nn.Module):
 
 	def forward(self,x):
 		x = torch.sigmoid(self.fc1(x))
-		x = torch.sigmoid(self.fc1(x))
+		x = torch.sigmoid(self.fc2(x))
 		return x
 
 model =  XOR_MLP()
 print(model)
 	
+# dataset
+
+dataset = [
+	(torch.tensor([0.0, 0.0]), torch.tensor([0.0])),
+	(torch.tensor([1.0, 0.0]), torch.tensor([1.0])),
+	(torch.tensor([0.0, 1.0]), torch.tensor([1.0])),
+	(torch.tensor([1.0, 1.0]), torch.tensor([0.0]))
+]
+
+# loss fn and optimizer
+
+criterion = nn.MSELoss()
+optimizer = optim.SGD(model.parameters(), lr = 0.89)
+
+# training loop
+
+for epochs in range(50000):
+
+	epoch_loss = 0.0
+
+	for inputs, target in dataset:
+		
+		# zero out gradients from previous step
+		optimizer.zero_grad()
+
+		# forward pass 
+		pred = model(inputs)
+
+		# loss 
+		loss = criterion(pred, target)
+		epoch_loss += loss.item()
+
+		# backward pass
+		loss.backward()
+
+		# optimizer step
+		optimizer.step()
+	
+	if epochs % 5000 == 0:
+		print(f"Epoch {epochs} || Total Loss: {epoch_loss:.4f}")
+
+print("="*150)
+
+print("Final Eval.")
+with torch.no_grad():
+	for inputs, target in dataset:
+		pred = model(inputs)
+
+		print(f"Inputs: {inputs.tolist()} | Target: {target.item()} | Guess: {pred.item():.4f}")
+		
