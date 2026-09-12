d1dd824211e5a7a155562717c95d98831035e1cd
Muntazir <muntazirjafri464@gmail.com>
2026-09-12 16:35:13 +0530

    Torch.nn MLP

--- xor_module_mlp.py
+++ xor_module_mlp.py
@@ -6,7 +6,7 @@ class XOR_MLP(nn.Module):
 	def __init__(self):
 		super().__init__()
 		self.fc1 = nn.Linear(2,2)
-		self.fc1 = nn.Linear(2,1)
+		self.fc2 = nn.Linear(2,1)
 
 	def forward(self,x):
 		x = torch.sigmoid(self.fc1(x))
