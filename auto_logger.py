e66a6a1bc240160fb10569e390a909dd5fc7fe69
Muntazir <muntazirjafri464@gmail.com>
2026-09-18 18:24:15 +0530

    Training Logs Tracker

--- auto_logger.py
+++ auto_logger.py
@@ -2,4 +2,7 @@ import datetime
 import csv
 
 def log_message(model, loss, epoch):
-    
+    time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    with open('training_log.csv', mode = 'a', newline='') as file:
+        csv_writer = csv.writer(file)
+        csv_writer.writerow([time, epoch, loss.item(), model.state_dict()])
