9bfab0dbfe332f95f1963c4507b89ffcdcea3eea
Muntazir <muntazirjafri464@gmail.com>
2026-09-19 15:08:19 +0530

    All files completely organised in download folder

--- file_organizer.py
+++ file_organizer.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 
-dict = {"JPEG" : "IMAGES", "PNG" : "IMAGES", "JPG" : "IMAGES", "GIF" : "IMAGES", "TXT" : "DOCUMENTS", "PDF" : "DOCUMENTS", "CSV" : "CSV FILES", "PY" : "PYTHON FILES", "MP3" : "AUDIO FILES", "MP4" : "VIDEO FILES", "MKV" : "VIDEO FILES", "EXE" : "EXECUTABLES", "ZIP" : "ARCHIVES", "RAR" : "ARCHIVES", "DOCX" : "DOCUMENTS", "PPTX" : "DOCUMENTS", "XLSX" : "SHEETS"}
+mapping = {"JPEG" : "IMAGES", "PNG" : "IMAGES", "JPG" : "IMAGES", "GIF" : "IMAGES", "TXT" : "DOCUMENTS", "PDF" : "DOCUMENTS", "CSV" : "CSV FILES", "PY" : "PYTHON FILES", "MP3" : "AUDIO FILES", "MP4" : "VIDEO FILES", "MKV" : "VIDEO FILES", "EXE" : "EXECUTABLES", "ZIP" : "ARCHIVES", "RAR" : "ARCHIVES", "DOCX" : "DOCUMENTS", "PPTX" : "DOCUMENTS", "XLSX" : "SHEETS"}
 
 target_dir = os.path.expanduser("~/Downloads")
 
@@ -13,8 +13,11 @@ for file in os.listdir(target_dir):
         file_ext = os.path.splitext(file)[1][1:].upper()
 
         # check if the file extension is in the dictionary
-        if file_ext in dict:
-            folder_name = dict[file_ext]
+        if file_ext in mapping:
+            folder_name = mapping[file_ext]
             folder_path = os.path.join(target_dir, folder_name)
 
             os.makedirs(folder_path, exist_ok=True)
+
+            # move the file to the corresponding folder
+            shutil.move(file_path, os.path.join(folder_path, file))
